0ab2f2f00e0e65c12bdb92fa1acb487cc1ca9e00
Chalermwat <zi.sk134@gmail.com>
2018-11-26 12:10:44 +0700

    Handle err
    server.js

--- server.js
+++ server.js
@@ -47,7 +47,9 @@ app.post("/applymembership",function(req,res){
     var inp=req.body;
     console.log(inp);
     var q = "INSERT INTO `goodboigoodcar`.`customer` (`ID`,`First_Name`, `Last_Name`, `Email`, `Tel`, `Address`, `Birthday`, `Membership`, `Number_of_Vehicle`) VALUES ('"+inp.CID+"','"+inp.First_Name+"', '"+inp.Last_Name+"', '"+inp.Email+"', '"+inp.Tel+"', '"+inp.Address+"', '"+inp.Birthday+"', '1', '"+inp.Number_of_Vehicle+"')"
-    connection.query(q);
+    connection.query(q,function(err,result){
+        if(err) res.sendFile(path.join(__dirname+"/HTML/login.html"));
+    });
     console.log("Insert to query");
 })
 
